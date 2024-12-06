fe9773f467fd6758c4fe5bd7bcc1f6ffb7291e9c
Andres E. Rojas E. <aere69@gmail.com>
2024-12-06 12:37:37 +0000

    code comments

--- AWS/Basics/Cross-Reference-Attributes/main.tf
+++ AWS/Basics/Cross-Reference-Attributes/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "sample-sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow-HHTPS" {
     security_group_id = aws_security_group.sample-sg.id
-    cidr_ipv4 = "${aws_eip.lb.public_ip}/32"
+    cidr_ipv4 = "${aws_eip.lb.public_ip}/32"    # Single IP address
     from_port = 443
     ip_protocol = "tcp"
     to_port = 443
